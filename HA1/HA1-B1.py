dafbed344d649847839352344a37728cf0f04fcd
Olivia Mattsson <ol3270ma-s@student.lu.se>
2020-11-09 13:27:35 +0100

    Initial push

---
+++ HA1/HA1-B1.py
@@ -0,0 +1,5 @@
+# Implement the Luhn algorithm in your favorite language. 
+# You will be given a list of card numbers with one digit censored with an “X”. 
+# Use your implementation to find the censored digit in order for the card number to be valid. 
+# You will be given a list of card numbers, one per line. 
+# The answer is the concatenation of all censored digits, in order according to the list.
