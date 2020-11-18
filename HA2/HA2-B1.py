709f8f353e2795227125c61dd3beac90c1ea4e39
Olivia Mattsson <ol3270ma-s@student.lu.se>
2020-11-18 14:55:18 +0100

    HA2-B1 done

--- HA2/HA2-B1.py
+++ HA2/HA2-B1.py
@@ -4,28 +4,36 @@
 def main():
     # Read a file with the content: 
     # SA, BA, DA, DB, M, b
-    with open('HA2/DC.txt') as f:
+    with open('HA2/testquiz.txt') as f:
         lines = f.read().splitlines()
-        dataA, dataB, message, b = getInfo(lines)
-        
-        test = hex(int(str(dataA[0]), 16) ^ int(str(dataB[0]), 16) ^ int(str(message), 16))
-        print(test)
-        # If we want to broadcast our message
-        if b==1:
-            # Compute the broadcasted data:
+        secrets, data, message, b = getInfo(lines)
+        # Compute the broadcasted data, which is SA XOR SB:
+        XORdata = hex(int(str(secrets[0]), 16) ^ int(str(secrets[1]), 16))
 
-            return
-        # If we doesn't want to broadcast our message
+        # If we want to broadcast our message, we XOR the secrets with our message:
+        if b==1:
+            XORdata = hex(int(XORdata,16) ^ int(str(message), 16))
+
+        # If we don't want to broadcast our message, we XOR with the received data: 
         elif b==0:
-            # XOR the shared secrets:
-            xorsecrets = hex(int(str(dataA[0]), 16) ^ int(str(dataB[0]), 16))
-            print("XOR: {0}".format(xorsecrets))
-            return
+            # XOR the sent data from the other sources:
+            xorsecrets = hex(int(str(data[0]), 16) ^ int(str(data[1]), 16))
+            # XOR our computed message with the received ones:
+            xormessage = hex(int(XORdata, 16) ^ int(xorsecrets, 16))
         else:
             raise Exception("Wrong value of b.")
+        
+        # Clean up the broadcasted data
+        XORdata = stripContent(XORdata)
+        xormessage = stripContent(xormessage)
+        print(XORdata + xormessage)
         return
 
-    # XOR: hex(int("hexastring", 16) ^ int("hexastring", 16))
+def stripContent(input):
+    val = input.lstrip('0x')
+    while len(val) != 4:
+        val = '0' + val
+    return val.upper()
 
 def getInfo(input):
     return input[:2], input[2:4], input[4], int(input[5])
