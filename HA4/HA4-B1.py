ebf3795150b590a4e079620955cd1b4c8c873dc0
Olivia Mattsson <ol3270ma-s@student.lu.se>
2020-12-04 11:31:41 +0100

    Working on B1 HA4

--- HA4/HA4-B1.py
+++ HA4/HA4-B1.py
@@ -4,74 +4,91 @@ import math
 import hashlib
 
 # Länk till RFC: https://tools.ietf.org/html/rfc8017
+hLen = 20
 
 def main():
-    # Read a file with the content: 
-    with open('HA4/testfiles/b1.txt') as f:
-        lines = f.read().splitlines()
-    
+    m = "c107782954829b34dc531c14b40e9ea482578f988b719497aa0687"
+    seed = "1e652ec152d0bfcd65190ffc604c0933d0423381"
+    encMessage = "0063b462be5e84d382c86eb6725f70e59cd12c0060f9d3778a18b7aa067f90b2178406fa1e1bf77f03f86629dd5607d11b9961707736c2d16e7c668b367890bc6ef1745396404ba7832b1cdfb0388ef601947fc0aff1fd2dcd279dabde9b10bfc51efc06d40d25f96bd0f4c5d88f32c7d33dbc20f8a528b77f0c16a7b4dcdd8f"
     # RSAES-OAEP Scheme
+    res = MGF1('9b4bdfb2c796f1c16d0c0772a5848b67457e87891dbc8214', 21)
+    print("MGF1: {0}".format(res))
+    print("-------------------------")
 
-    # RSAEP primitive (Section 5.1.1)
-    c = RSAEP(pubKey, m)
-    # RSADP primitive (Section 5.1.2)
-    message = RSADP(privKey, c)
-    # EME-OAEP encoding method (Section 7.1)
-        # EME-OAEP encoding: Step 2 in 7.1.1
-        
-        # EME-OAEP decoding: Step 3 in 7.1.2
+    # EME-OAEP encoding: Step 2 in 7.1.1
+    c = OAEPencode(seed, m)
+    print("Encrypted message: " + c)
+    print("-------------------------")
+    # EME-OAEP decoding: Step 3 in 7.1.2
+    decrypted = RSADP(encMessage)
+    print("Decrypted message: " + decrypted)
+            
+    
 
+def OAEPencode(seed, message):
+    lHash = hashlib.sha1(bytearray("".encode())).hexdigest()
+    mLen = int(len(message)/2)
+    k = 128
+    PS = (k - mLen - 2*hLen) - 2
+    padding = "".zfill((PS*2))
+    DB = lHash + padding + "01" + message
+    dbMask = MGF1(seed, (k - hLen - 1))
+    maskedDB = hex(int(DB,16) ^ int(dbMask, 16))[2:]
+    seedMask = MGF1(maskedDB, hLen).lstrip("0x")
+    maskedSeed = hex(int(seed,16) ^ int(seedMask, 16))[2:]
+    encMessage = "00" + maskedSeed + maskedDB
+    return encMessage
+
+def OAEPdecode():
+    # TODO
+    return
 
 
 # Mask Genereation Function
 def MGF1(mgfSeed, maskLen):
-    # Since we use SHA1:
-    hLen = 40
     # If maskLen > 2^32 hLen, output "mask too long" and stop.
-    if maskLen > 2**32 * hLen:
+    if maskLen > (2**32) * hLen:
         print("Mask too long")
         return
     #  Let T be the empty octet string (byte string)
     T = ""
     # For counter from 0 to \ceil (maskLen / hLen) - 1:
-    for i in range(math.ceil(maskLen/hLen) - 1):
+    for i in range(0,math.ceil(maskLen/hLen)):
         # Convert counter to an octet string C of length 4 octets:
         C = I2OSP (i, 4)
-
+        
         # Concatenate the hash of the seed mgfSeed and C to the octet string T:
         T += sha_hash(mgfSeed + C) 
-    return T
+    
+    # Output the leading maskLen octets of T as the octet string mask.
+    
+    return T[:2*maskLen]
 
-# IOSP function
+# I20SP function
 def I2OSP(x, xLen):
 
     # If x >= 256^xLen, output "integer too large" and stop.
-    if x > 256**xLen:
+    if x >= 256**xLen:
         print("Integer too large")
         return
     # Make a representation of X in base 256: 
     X = ""
-    for i in range(xLen):
-        X += x * 256**xLen
+    for i in range(1,xLen+1):
+        if i == xLen:
+            res = x
+        elif (x / (256**(xLen-i))) > 1:
+            res = int(x - math.floor(x / (256**(xLen-i))))
+            x = x - res        
+        else:
+            res = "0"
+        X += str(res)
     # Return the base 256 X value:
-    return X
-
-# RSAEP primitive (Section 5.1.1)
-def RSAEP(pubKey, m):
-    n, e = pubKey
-    c = ""
-    return c
-
-# RSADP primitive (Section 5.1.2)
-def RSADP(privKey, c):
-    n, d, quint, c = privKey
-    m = ""
-    return m
+    return (X.zfill(2*xLen))
 
 
 # Byte array to hash
 def sha_hash(inputVal):
-    h = hashlib.sha1(inputVal).hexdigest()
+    h = hashlib.sha1(bytearray.fromhex(inputVal)).hexdigest()
     return h
 
 if __name__ == '__main__':
---
+++ HA4/comparison.py
@@ -0,0 +1,85 @@
+import hashlib
+from math import ceil
+import random
+
+
+hLen = 20
+k = 128
+
+
+def I20SP (x, xLen):
+    if x >= 256**xLen:
+        return "integer too large"
+    return (hex(x)[2:].zfill(2*xLen))
+
+
+
+def MGF1(mgfSeed, maskLen):
+    if maskLen >= 2**32 * hLen:
+        return "mask too long"
+    T = ""
+    for i in range(0, int(ceil(maskLen / hLen))):
+        C = I20SP (i, 4)
+        T = T + hashlib.sha1(bytearray.fromhex(mgfSeed + C)).hexdigest()
+                
+    return T[:2*maskLen]
+
+
+def OAEP_encode(M, seed):
+    if len(M) > (k - 2 * hLen - 2):
+        return "message too long"
+    L = ""
+    lHash = hashlib.sha1(bytearray(L.encode())).hexdigest()
+    PS = "".zfill(((k - int(len(M)/2) - 2*hLen) - 2)*2)
+    print("PS: " + PS)
+    DB = lHash + PS + "01" + M 
+    print("DB: " + DB)
+    random_octet = str(bytearray(random.getrandbits(8) for _ in range(hLen)))
+    dbMask = MGF1(seed, k - hLen - 1)
+    print("dbMask: " + dbMask)
+    maskedDB = hex(int(DB, 16) ^ int(dbMask, 16))[2:]
+    print("maskedDB: " + maskedDB)
+    seedMask = MGF1(maskedDB, hLen)
+    print("seedMask: " + seedMask)
+    maskedSeed = hex(int(seed, 16) ^ int(seedMask, 16))[2:]
+    print("maskedSeed: " + maskedSeed)
+    EM = "00" + maskedSeed + maskedDB
+    return EM
+
+def OAEP_decode (EM):
+    L = ""
+    lHash = hashlib.sha1(bytearray(L.encode())).hexdigest()
+   
+    Y = EM[:2]
+    maskedSeed = EM[2:2 * hLen + 2]
+    maskedDB = EM[2 * hLen + 2:]
+    seedMask = MGF1(maskedDB, hLen)
+   
+    seed = hex(int(maskedSeed, 16) ^ int(seedMask, 16))[2:]
+    dbMask = MGF1(seed, k - hLen - 1)
+    DB = hex(int(maskedDB, 16) ^ int(dbMask, 16))[2:]
+   
+    lPrimeHash = DB[:hLen]
+    PS = DB[hLen * 2: hLen * 2 + DB[hLen * 2:].find("01") + 2]
+    M = DB[hLen * 2 + len(PS):]
+
+    return M
+
+
+    
+if __name__ == '__main__':
+  
+    mgfSeed = "d9918d2cd546940c8b3beccd09c5e1d58c72b4998c7f52a5f267"
+    maskLen = 26
+
+    M = "c107782954829b34dc531c14b40e9ea482578f988b719497aa0687"
+    seed = "1e652ec152d0bfcd65190ffc604c0933d0423381"
+    
+    
+    EM = "00581bc2381cf79218566065eb1def452262df368e129de319b5c2bb66e84df6be244fc653a9468c6aafbe715fe366526e9596c452cdf7a42ddcec8d8005724dc7d9450b769aa0fe6f58e8949e503294de3106a7a3b0254eac2b94d245421e610ca70466137c29e7ff5ccd41dda83a44457ea3c820d0f360599833d34ec82e3b"
+  
+    print("MGF1 output: " + MGF1(mgfSeed, maskLen))
+    print("<==============================================================>")
+    print("OAEP encode output: " + OAEP_encode(M, seed)) 
+    print("<==============================================================>")
+    print("OAEP decode output: " + OAEP_decode(EM)) 
