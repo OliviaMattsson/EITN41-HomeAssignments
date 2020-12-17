5112113424ac5d672727e42713a64cdc8cd48ff9
Olivia Mattsson <ol3270ma-s@student.lu.se>
2020-12-17 13:14:05 +0100

    B2 maybe done?

--- HA5/HA5-B2.py
+++ HA5/HA5-B2.py
@@ -3,17 +3,17 @@
 import hashlib
 
 # Inputs:
-identity = 'walter@crypto.sec'
+identity = 'wendy@crypto.sec'
 p = '9240633d434a8b71a013b5b00513323f'
 q = 'f870cfcd47e6d5a0598fc1eb7e999d1b'
-encryptedBits = ['0b62a946b76845e44591e79a9fe1f8a2d44095fad9d3543a570f65a2168b5051',
-'2bdd5d58fe150fc75ad2f6f0858f0df3128597b1e06bdde3cbeb7605f5b39dcd',
-'7911234bca14066caf7831449a8dbdef32c7b24c53e948765c0e803da77353a3',
-'7e9c90937d6eb21f93fd4cf37d3ee126e6eb5e600d05ae3a58816db60c3fb74b',
-'5d4e9a468e76b863d3969d7f844752545eac0baabebdf10b42e9e679b5ce15a5',
-'52f9c7c50a00cee060ce12012b7fadb094ea0d920ff4d3f0fc2c8a92afea8020',
-'5d1717c2eebadd66a3192ce361e1ede546d96db9d287c722528d3f4932d8c640',
-'19a3ebbdf8b31209200c24b76cc3735d7f12cdc1b544b80bc995f3bcb1836eda']
+encryptedBits = ['7a3de06528072d38fccd30c52ebae6536b0fa6d86459fc00157fae085aac91c8',
+'586af7c3e0a10f41672b55120a21b8962b1467128ebb4a27780c36197ba1f827',
+'4f75cf2ed24ad656643c4778b33f08a14cc88380c79c93a20979c68030c5fe9c',
+'52cebec5b8e276560649507a641094c0018ef010e809acb256604f280ed46694',
+'231519968e4585b410e4840e2e0b3a442e69679e765272711c9639d53e3017fe',
+'6e72f5b2cde9e03c9431e6685cfa62f4254f6fe8d07b2d3b196fbab44aa99164',
+'396b1effbb8f05a15d82c9424737588d7bdd2f8b87acae5c626c66822bbc5b4b',
+'093485f019dda827ca54df573918fcb9eeb51734772aa85bd76028661cb5794a']
 
 def main():
     M= int(p,16)*int(q,16)
@@ -26,6 +26,7 @@ def main():
             foundHash = True
     r= calculateRoot(int.from_bytes(hashedEmail,byteorder= 'big'), M)
     message = decrypt(r, encryptedBits, M)
+    print(message)
     return int(message,2), hex(r).lstrip('0x')
 
 # Using Cocks encryption scheme - PKG
@@ -44,6 +45,7 @@ def decrypt(r, m, M):
             l+= '1'
     return l 
     
+# Taken from assignment page: 
 def jacobi (a, m):
     j = 1
     a %= m
@@ -70,5 +72,4 @@ if __name__ == '__main__':
     m,r = main() 
     print('message: {0}'.format(m))
     print('root: {0}'.format(r))
-   
     
