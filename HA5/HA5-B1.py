f93661b1e3482f68f175e2c6780120da99785f3c
Olivia Mattsson <ol3270ma-s@student.lu.se>
2020-12-17 13:09:11 +0100

    B1 done

--- HA5/HA5-B1.py
+++ HA5/HA5-B1.py
@@ -5,7 +5,6 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5
 from Crypto.Hash import SHA
 from Crypto import Random
-# from OpenSSL import crypto
 import pyasn1.codec.der.decoder
 import pyasn1.codec.der.encoder
 from pyasn1_modules import rfc8017
@@ -25,7 +24,7 @@ def decrypt(m, privKey):
 # Using https://pycryptodome.readthedocs.io/en/latest/src/public_key/rsa.html to construct the private key
 # And retrieve information about the RSA components:
 def reconstructKey():
-    with open('HA5/key.pem', 'r') as pem_file:
+    with open('key.pem', 'r') as pem_file:
         k = ''.join(pem_file.readlines()[1:-1])
         pem_key = base64.b64decode(k)
         # Decode using PKCS #1 - the format used in RSA private keys:
@@ -38,14 +37,13 @@ def reconstructKey():
         newN = p * q
         decoded_key[1] = newN
         new_key = RSA.importKey(pyasn1.codec.der.encoder.encode(decoded_key))
-    
 
     return new_key
     
 
 if __name__ == '__main__':
     k = reconstructKey()
-    encrypted_message = 'T9FAfFVcVCdPH45kv3OU/Kot9NOyQ2t5tWI1GW6nJ4Ul435T68wq1f1vm3KhDcKONzdN3krJ/VwlIzdssIcqmVizw5mnMupmd1gNmf7EKLZWjT4LaMQhDMijrfhxCdbiQKjKqYnUehlOCeDS0JXOJpiYcCtbmTVYHBmxBuOZ1l8='
+    encrypted_message = 't2JtdjaM71d67nvC9CZZ5kpumAmY9LrEh8//OdUKX+xvv+UG+9tvM/9P/Aen/tW21FFfNUWPKm+EkuHjecvMa5KqZqVoXKNqVz4Ke4p1fL1eVdUpJ8Rnj5XoI9Bg2RVK0ZgTqsllUzKzaTfrkvQcya+qUTLCruT+JeRgdrsdTKE='
     encrypted_message = base64.b64decode(encrypted_message)
     m = decrypt(encrypted_message, k)
     print(m)
